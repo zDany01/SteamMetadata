c3a48d07ed52f5ae4908163f83cc27f92224d83d
zDany01 <85403430+zDany01@users.noreply.github.com>
2024-11-30 19:33:38 +0100

    Added GameData class
    Added
     - Json.NET external library

---
+++ GameData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamMetadata
+{
+    internal class GameData
+    {
+        [JsonProperty("name")]
+        public string Name { get; }
+        [JsonProperty("appid")]
+        public int AppID { get; }
+
+        public GameData(string name, int appID)
+        {
+            this.Name = name;
+            this.AppID = appID;
+        }
+
+        public Image GetCapsule() => throw new NotImplementedException();
+        public Image GetHero() => throw new NotImplementedException();
+        public Image GetLogo() => throw new NotImplementedException();
+    }
+}
