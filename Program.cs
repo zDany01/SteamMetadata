4a29f72b28637f8f054f3f5354ab60657028f6f3
Michele Guarino <110943571+michele-gua@users.noreply.github.com>
2024-11-30 17:19:08 +0100

    Refactored APP_NAME variable
    Refactored
     - APP_NAME variable scope

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ namespace SteamMetadata
 {
     internal static class Program
     {
+        public const string APP_NAME = "Steam Metadata";
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            const string APP_NAME = "Steam Metadata";
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GUI());
