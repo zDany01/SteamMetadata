0816e9402d3e4f621ea7d5c2b49f1aa191c9f773
zDany01 <85403430+zDany01@users.noreply.github.com>
2024-11-30 17:03:30 +0100

    Initial GUI Structure

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SteamMetadata
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            const string APP_NAME = "Steam Metadata";
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GUI());
+        }
+    }
+}
